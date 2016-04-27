46824dcef1036f20e801976d5f9d52630161a867
Matt T <MMT@Matts-MacBook-Pro.local>
2016-04-27 16:08:56 -0400

    renamed a spec

--- spec/features/worker/worker_spec.rb
+++ spec/features/worker/worker_spec.rb
@@ -1,10 +1,10 @@
 require "rails_helper"
 require 'capybara/poltergeist'
 require 'database_cleaner'
-Capybara.javascript_driver = :selenium
+Capybara.javascript_driver = :poltergeist
 
 Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, {js_errors: true})
+  Capybara::Poltergeist::Driver.new(app, {js_errors: false})
 end
 
 feature "worker logs in" do
@@ -37,5 +37,39 @@ feature "worker logs in" do
     click_button 'Search'
     sleep 1
     expect(page).to have_content('nails')
+  end
+
+  scenario "worker signs up for a job", js: true do
+    visit root_path
+    fill_in 'worker[email]', with: "worker1@worker.com"
+    fill_in 'worker[password]', with: "-worker1"
+    page.execute_script("$( '#sign-in-button' ).click()")
+    sleep 1
+    click_link 'search-link'
+    fill_in 'search', with: 'nails'
+    click_button 'Search'
+    sleep 2
+    find('div.post').click
+    sleep 2
+    click_button 'SIGN UP'
+    sleep 1
+    expect(page).to have_content('Construction Job Title')
+  end
+
+  scenario "worker resigns from a job", js: true do
+    visit root_path
+    fill_in 'worker[email]', with: "worker1@worker.com"
+    fill_in 'worker[password]', with: "-worker1"
+    page.execute_script("$( '#sign-in-button' ).click()")
+    sleep 1
+    click_link 'search-link'
+    fill_in 'search', with: 'Plowing'
+    click_button 'Search'
+    sleep 2
+    find('div.post').click
+    sleep 2
+    click_button 'RESIGN'
+    sleep 1
+    expect(page).to have_content('You have resigned from a job! Plowing Job Title')
   end
 end
