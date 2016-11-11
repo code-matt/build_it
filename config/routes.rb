41a0a0576f02992166c2bca6625adfe780209ff5
Matt T <code.matthew.thompson@gmail.com>
2016-11-11 14:54:16 -0500

    sets up knock gem jwt authentication and basic user class on rails side

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # post 'user_token' => 'user_token#create'
+  namespace :api do
+    namespace :v1 do
+      mount Knock::Engine => "/knock"
+      resources :users, only: [:create]
+    end
+  end
 end
