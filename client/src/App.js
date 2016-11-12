1ece3c1967efb6b7f47f696c485f0fc844749b6d
Matt T <code.matthew.thompson@gmail.com>
2016-11-11 20:10:53 -0500

    Dashboard component is now a protected route on the react side

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react'
 import NavBar from './components/navbar/navbar'
 import './App.css'
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import Login from './components/login/login'
 import Dashboard from './components/dashboard/dashboard'
 import { Router, Route, hashHistory } from 'react-router'
+import _authService from './network/auth'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      loggedIn: false
+    }
+  }
   render () {
     return (
       <div>
         <Router history={hashHistory}>
-          <Route path="/" component={Login}/>
-          <Route path="/dashboard" component={Dashboard}/>
+          <Route path='/' component={Login} />
+          <Route path='/dashboard' component={Dashboard} onEnter={requireAuth} />
         </Router>
       </div>
-
     )
+  }
+}
+function requireAuth(nextState, replace) {
+  if (!_authService.loggedIn()) {
+    replace({
+      pathname: '/',
+      state: { nextPathname: nextState.location.pathname }
+    })
   }
 }
 
