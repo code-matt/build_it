e6b2cf41b8020fea4f2bbe46e2923ac880da5130
Matt T <code.matthew.thompson@gmail.com>
2016-11-15 12:27:21 -0500

    Added carrierwave configured for an aws bucket as well as a unit test for it. Going to need this later

--- spec/unit/carrierwave_spec.rb
+++ spec/unit/carrierwave_spec.rb
@@ -2,7 +2,7 @@ require_relative '../rails_helper.rb'
 require_relative '../spec_helper.rb'
 
 describe 'carrierwave' do
-  it 'should upload files' do
+  it 'should successfully upload files' do
     a = User.create(email: "qq@qq.com", password: "12345678")
     src = File.join(Rails.root,"client/public/favicon.ico")
     file = File.new(src)
