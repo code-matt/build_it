d014be8ed6ac53cbabff8a5df545e4331fe09034
Matt T <code.matthew.thompson@gmail.com>
2016-11-16 16:56:30 -0500

    started to add profile picture upload

--- client/src/components/signup/profile-form.jsx
+++ client/src/components/signup/profile-form.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import _authService from '../../network/auth'
 import Notifications, {notify} from 'react-notify-toast'
+import Dropzone from 'react-dropzone'
+import request from 'superagent';
 
 class ProfileForm extends Component {
   constructor () {
@@ -14,13 +16,35 @@ class ProfileForm extends Component {
     console.log('profile submitted')
   }
 
+  onImageDrop (files) {
+    let upload = request.post('http://localhost:3000/api/v1/profilepic')
+                        .field('file', files[0])
+
+    upload.end((err, response) => {
+      if (err) {
+        console.error(err)
+      }
+
+      if (response.body.secure_url !== '') {
+        console.log(response.body)
+      }
+    })
+  }
+
   render () {
     return (
       <div>
         We need to know a little more about you..
+        <Dropzone
+          multiple={false}
+          accept="image/*"
+          onDrop={this.onImageDrop.bind(this)}>
+          <p>Drop an image or click to select a file to upload.</p>
+        </Dropzone>
         <form onSubmit={this.handleSubmit}>
           <label><input ref='firstName' placeholder='firstName' defaultValue='First Name' /></label>
-          <label><input ref='lastName' placeholder='lastName' defaultValue='Last Name'  /></label><br />
+          <label><input ref='lastName' placeholder='lastName' defaultValue='Last Name'  /></label>
+          <label><input ref='location' placeholder='location' defaultValue='Location'  /></label><br />
           <button type='submit'>Submit</button>
         </form>
       </div>
