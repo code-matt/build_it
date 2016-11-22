5b32349a633375ae8d96c737254919966773e732
Matt T <code.matthew.thompson@gmail.com>
2016-11-21 19:09:45 -0500

    Styles contracts

--- client/src/components/jobs/view-contract.jsx
+++ client/src/components/jobs/view-contract.jsx
@@ -13,23 +13,43 @@ class Contract extends Component {
     return (
       <div>
         {this.state.contract
-          ? <div>
-            <div>
-              {this.state.selectedJob.title}
-              <br />
-              {this.state.selectedJob.description}
-              <br />
-              {this.state.contract.proposal
-                ? this.state.contract.proposal
-                : null}
-              <br />
-            </div>
-            <div>
-              {this.state.contract.accepted
-                ? 'accepted'
-                : 'not accepted'}
+        ? <div className='container'>
+          <div className='row'>
+            <div className='col-md-12'>
+              <div className='card'>
+                <div className='card-header'><h4>{this.state.selectedJob.title}</h4></div>
+                <div className='card-block'>
+                  <div className='row'>
+                    <div className='col-md-3 profileimg'>
+                      <img className='img-circle' src={'https://builditreact.s3.amazonaws.com/uploads/user/avatar/' + this.state.selectedJob.user_id + '/image.png'} />
+                    </div>
+                    <div className='col-md-9'>
+                      <p className='card-text'>{this.state.selectedJob.description}</p>
+                    </div>
+                  </div>
+                  <hr />
+                  <ul className='list-group list-group-flush'>
+                    <li className='list-group-item'><i className='fa fa-map-marker fa-3x' aria-hidden='true' /><span className='details-li'>{this.state.selectedJob.address}</span></li>
+                    <li className='list-group-item'><i className='fa fa-money fa-3x' aria-hidden='true' /><span className='details-li'>{this.state.selectedJob.hourly_rate / 100 + '$/hr'}</span></li>
+                  </ul>
+                  <div className='alert alert-success text-md-center'>
+                    Proposal
+                    <hr />
+                    {this.state.contract.proposal}
+                    <hr />
+                  </div>
+                  {this.state.contract.accepted
+                    ? <div className='alert alert-success text-md-center' role='alert'>
+                      <strong>Your proposal has been accepted! Check your jobs dashboard</strong>
+                    </div>
+                    : <div className='alert alert-warning text-md-center' role='alert'>
+                      <strong>Pending<br />Your proposal has been sent but is waiting to be accepted or denied by this jobs owner.</strong>
+                    </div>}
+                </div>
+              </div>
             </div>
           </div>
+        </div>
         : null}
       </div>
     )
