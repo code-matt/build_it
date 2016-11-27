f6939e565f4b4cdf18cab3a2d656e6b56a0a2e54
Matt T <code.matthew.thompson@gmail.com>
2016-11-27 00:10:40 -0500

    works on edit profile modal's styling

--- client/src/components/modals/finish-profile-guard.jsx
+++ client/src/components/modals/finish-profile-guard.jsx
@@ -62,35 +62,53 @@ class FinishProfileModal extends Component {
             <div className='container'>
               <div className='row'>
                 <div className='col-md-12'>
-                  <div className='pull-right'>
-                    <button onClick={this.handleToggle} className='closebutton btn btn-danger' href='#'>
-                      <i className='fa fa-window-close-o' /> Close</button>
-                  </div>
-                  <div className='text-md-center profileform'>
-                    We need to know a little more about you..
-                    <Dropzone
-                      className='dropzone'
-                      multiple={false}
-                      accept='image/*'
-                      onDrop={this.onImageDrop.bind(this)}>
-                      {this.props.profile.picUrl
-                      ? <div>
-                        <img className='img-fluid' src={this.props.profile.picUrl} />
+                  <div className='card'>
+                    <div className='card-header'>
+                      <div className='pull-right'>
+                        <button onClick={this.handleToggle} className='closebutton btn btn-danger' href='#'>
+                          <i className='fa fa-window-close-o' /> Close</button>
                       </div>
-                      : <div>
-                        <h5 className='text-md-center'>Click here or drop image to add profile picture</h5>
-                      </div>}
-                    </Dropzone>
-                    { renderErrors(this.props.errors.profile, 'avatar') }
-                    <form onSubmit={this.handleProfileEdit}>
-                      <label><input onChange={this.handleValueChange} id='firstName' ref='firstName' placeholder='First Name' defaultValue={this.props.profile.firstName} /></label>
-                      { renderErrors(this.props.errors.profile, 'first_name') }
-                      <label><input onChange={this.handleValueChange} id='lastName' ref='lastName' placeholder='Last Name' defaultValue={this.props.profile.lastName} /></label>
-                      { renderErrors(this.props.errors.profile, 'last_name') }
-                      <label><input onChange={this.handleValueChange} id='location' ref='location' placeholder='Location' defaultValue={this.props.profile.location} /></label><br />
-                      { renderErrors(this.props.errors.profile, 'location') }
-                      <button className={'btn btn-primary searchbtn'} type='submit'>Submit</button>
-                    </form>
+                      <h4>Edit Profile</h4>
+                    </div>
+                    <div className='card-block'>
+                      <div className='text-md-center profileform'>
+                        We need to know a little more about you..
+                        <Dropzone
+                          className='dropzone'
+                          multiple={false}
+                          accept='image/*'
+                          onDrop={this.onImageDrop.bind(this)}>
+                          {this.props.profile.picUrl
+                          ? <div>
+                            <img className='img-fluid' src={this.props.profile.picUrl} />
+                          </div>
+                          : <div>
+                            <h5 className='text-md-center'>Click here or drop image to add profile picture</h5>
+                          </div>}
+                        </Dropzone>
+                        { renderErrors(this.props.errors.profile, 'avatar') }
+                        <form onSubmit={this.handleProfileEdit}>
+                          <ul className='list-group list-group-flush'>
+                            <div class='input-group'>
+                              <span class='input-group-addon' id='basic-addon1'><i className='fa fa-id-badge fa-2x' aria-hidden='true' /></span>
+                              <input onChange={this.handleValueChange} id='firstName' ref='firstName' placeholder='First Name' defaultValue={this.props.profile.firstName} type='text' class='form-control' aria-describedby='basic-addon1' />
+                            </div>
+                            { renderErrors(this.props.errors.profile, 'first_name') }
+                            <div class='input-group'>
+                              <span class='input-group-addon' id='basic-addon2'><i className='fa fa-id-badge fa-2x' aria-hidden='true' /></span>
+                              <input onChange={this.handleValueChange} id='lastName' ref='lastName' placeholder='Last Name' defaultValue={this.props.profile.lastName} type='text' class='form-control' aria-describedby='basic-addon2' />
+                            </div>
+                            { renderErrors(this.props.errors.profile, 'last_name') }
+                            <div class='input-group'>
+                              <span class='input-group-addon' id='basic-addon3'><i className='fa fa-id-badge fa-2x' aria-hidden='true' /></span>
+                              <input onChange={this.handleValueChange} id='location' ref='location' placeholder='Location' defaultValue={this.props.profile.location} type='text' class='form-control' aria-describedby='basic-addon2' />
+                            </div>
+                            { renderErrors(this.props.errors.profile, 'location') }
+                            <button className={'btn btn-primary searchbtn'} type='submit'>Submit</button>
+                          </ul>
+                        </form>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
